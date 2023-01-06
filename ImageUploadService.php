aba9e146defaa6dc2a20abc44de16d17f2a1d3bd
amirjavadikia <amir.m.javadikia@gmail.com>
2023-01-06 18:52:07 +0330

    first commit

---
+++ ImageUploadService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Services\ImageUpload;
+
+class ImageUploadService
+{
+    public static function upload($fileRequest, $ImageModel)
+    {
+        $file = $fileRequest;
+
+        $image_name = $file->getClientOriginalName();
+
+        $path_image = "image/example/".$image_name;
+
+        if (file_exists($path_image)) {
+            $image_name = time() . $image_name;
+        }
+
+        $targetFile = $path_image . $image_name;
+        $fileType = pathinfo($targetFile, PATHINFO_EXTENSION);
+
+        if ($fileType !== 'jpg' && $fileType !== 'png'){
+            return redirect()->back()->with('error', 'Image format is invalid');
+        }
+        else {
+            if($file->getSize() > 2000000){
+                return redirect()->back()->with('error', 'Image size is more than 2 mg');
+            }
+        }
+        $file->move("image/store/", $image_name);
+
+        $result = $ImageModel::create([
+            'image' => $image_name
+        ]);
+
+        if($result){
+            return redirect()->back()->with('upload', 'Image was uploaded successful');
+        }
+    }
+}
